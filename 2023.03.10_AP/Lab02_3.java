1e0f94568acb55ef2bd0a7569fd5e6db6da6c729
Aleksandra Połacik <polacik.ola@gmail.com>
2023-03-14 12:01:56 +0100

    zad3

---
+++ 2023.03.10_AP/Lab02_3.java
@@ -0,0 +1,8 @@
+public class Lab02_3 {
+    public static void main(String args[]){
+        System.out.println("Typ nadrzędny dla String: klasa Object");
+        System.out.println("Typ nadrzędny dla Scanner: klasa Object");
+        System.out.println("Typ nadrzędny dla ImageOutputStream: interfejs ImageOutput");
+    }
+}
+

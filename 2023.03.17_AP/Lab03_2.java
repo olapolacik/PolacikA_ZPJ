9fc1b753b416ad39efca647f3b91a8699407c62e
Aleksandra Połacik <polacik.ola@gmail.com>
2023-03-25 11:34:13 +0100

    zad2

--- 2023.03.17_AP/Lab03_2.java
+++ 2023.03.17_AP/Lab03_2.java
@@ -5,10 +5,8 @@ public class Lab03_2 {
         Runnable task3 = () -> System.out.println("Task3 został uruchomiony");
         Runnable task4 = () -> System.out.println("Task4 został uruchomiony");
 
-        // 
         System.out.println("runTogether");
         TaskRunner.runTogether(task1, task2, task3, task4);
-        // zadania wywołane w bieżącym wątku
         System.out.println("runInOrder");
         TaskRunner.runInOrder(task1, task2, task3, task4);
     }

6f8e72e2e46a85f249838a34ce97a8a30ca47a3a
Aleksandra Połacik <polacik.ola@gmail.com>
2023-03-31 12:30:50 +0200

    Lab05

---
+++ 2023.03.31_AP/RandomInts.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class RandomInts {
+    private static Random generator = new Random();
+
+    public static IntSequence randomInts(int low, int high) {
+        class RandomSequence implements IntSequence {
+            public int next() {
+                return low + generator.nextInt(high - low + 1);
+            }
+            public boolean hasNext() { return true; }
+        }
+        return new RandomSequence();
+    }
+
+    private interface IntSequence {
+        int next();
+        boolean hasNext();
+    }
+
+
+    public static void main(String[] args){
+        IntSequence seq = RandomInts.randomInts(1, 10);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(seq.next());
+        }
+    }
+}

734166eb0b236261759d8d0e4688b7a95bd6be0c
Aleksandra Połacik <polacik.ola@gmail.com>
2023-04-14 14:51:05 +0200

    m

---
+++ 2023.04.14_AP/Lab06_3.java
@@ -0,0 +1,40 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+
+public class Lab06_3 {
+    public static void main(String[] args){
+        try{
+            //zamiana pliku w strumien tokenów
+            Scanner sc = new Scanner(new File("alice.txt"));
+            sc.useDelimiter("\\P{L}+");
+            sc.tokens()
+                .filter(s -> s.matches("[a-zA-Z]+"))
+                .limit(100)
+                .forEach(System.out::println);
+
+            //wczytywanie pliku ponownie i wyswietlenie 10 najczesciej wystepujacych slow
+            sc = new Scanner(new File("alice.txt"));
+            sc.useDelimiter("\\P{L}+");
+            Map<String, Long> wordFrequencies = sc.tokens()
+                .filter(s -> s.matches("a-zA-Z+"))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(String::valueOf, Collectors.counting()
+            ));
+
+            wordFrequencies.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .forEach(entry -> System.out.println(entry.getKey() + " - " + entry.getValue()));
+
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+    }
+}

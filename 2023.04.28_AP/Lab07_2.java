68c40721d1cba0d25eb564ac627c4479cf0fd697
Aleksandra Połacik <polacik.ola@gmail.com>
2023-04-28 12:39:53 +0200

    lab07

--- 2023.04.28_AP/Lab07_2.java
+++ 2023.04.28_AP/Lab07_2.java
@@ -16,10 +16,9 @@ public class Lab07_2 {
 
     public static void main(String[] args){
 
-        Stream<Integer> pierwszy
-        Stream<Integer> drugi
-        Stream<Integer> wynikowy = Lab_07
-
+        Stream<Integer> pierwszy = Stream.of(1,3,5,7);
+        Stream<Integer> drugi = Stream.of(2,4,6);
+        Stream<Integer> wynikowy = Lab07_2.zip(pierwszy,drugi);
+        wynikowy.forEach(System.out::println);
     }
-    
 }
